fd9359e9b10d5d9165c29be39d9125317734393c
tobiu <tobiasuhlig78@gmail.com>
2020-06-23 22:42:40 +0200

    Table View: country flags

--- apps/covid/view/country/Table.mjs
+++ apps/covid/view/country/Table.mjs
@@ -46,7 +46,14 @@ class Table extends Container {
             width               : 200,
 
             renderer: data => {
-                return data.value;
+                return {
+                    cls : ['neo-country-column', 'neo-table-cell'],
+                    html: [
+                        '<div style="display: flex; align-items: center">',
+                            '<img style="height:20px; margin-right:10px; width:20px;" src="' + Util.getCountryFlagUrl(data.value) + '">' + data.value,
+                        '</div>'
+                    ].join('')
+                };
             }
         }, {
             dataField: 'cases',
